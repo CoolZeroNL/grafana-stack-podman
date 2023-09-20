f58706050ffd9b4427de63f2c1cb463a89464f44
CoolZeroNL <26223744+CoolZeroNL@users.noreply.github.com>
2023-09-20 08:04:39 +0200

    add

---
+++ collect.py
@@ -0,0 +1,42 @@
+import os, time, random
+import influxdb_client
+from influxdb_client import Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+try:
+    from gpiozero import CPUTemperature
+    cpu = CPUTemperature()
+    is_rpi = True
+except:
+    is_rpi = False
+
+# bucket = os.environ.get("INFLUXDB_BUCKET")
+# org = os.environ.get("INFLUXDB_ORG")
+# token = os.environ.get("INFLUXDB_TOKEN")
+
+bucket = "INFLUXDB_BUCKET"
+org = "INFLUXDB_ORG"
+token = "INFLUXDB_TOKEN"
+
+url = "http://127.0.0.1:8086"
+influx_client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
+
+write_api = influx_client.write_api(write_options=SYNCHRONOUS)
+
+while True:
+    if is_rpi:
+        val = cpu.temperature
+    else:
+        val = float(f"{random.random():.2}")
+    point = (
+        Point("measurement")
+        .tag("source", "cpu")
+        .field("temperature", val)
+    )
+    logger.info(f"{point}")
+    write_api.write(bucket=bucket, org=org, record=point)
+    time.sleep(1)
